d40311bedf730502c01d3b5141b50019859a40d8
saetovdinar <saetovdinar@users.noreply.github.com>
2024-10-28 10:04:18 +0000

    deploy: 9bff0a518dce993c30e072e724c405c61164a1ff

---
+++ _next/static/chunks/app/contacts/page-549eab6f79685323.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[613,945],{7782:function(e,n,s){Promise.resolve().then(s.t.bind(s,5878,23)),Promise.resolve().then(s.bind(s,8494)),Promise.resolve().then(s.bind(s,3182)),Promise.resolve().then(s.bind(s,2721))}},function(e){e.O(0,[948,552,661,971,117,744],function(){return e(e.s=7782)}),_N_E=e.O()}]);
